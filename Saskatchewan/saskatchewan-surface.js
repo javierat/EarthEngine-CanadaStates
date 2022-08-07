e8a682375298f1ad92fb4eea2304631de7a1f644
Javier Alejandro Torres <87143528+javierat@users.noreply.github.com>
2022-08-07 01:17:07 -0500

    Update saskatchewan-surface.js

--- Saskatchewan/saskatchewan-surface.js
+++ Saskatchewan/saskatchewan-surface.js
@@ -1,1 +1,225 @@
 
+var x1 = -102.0006
+var y1 = 60.0009
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+Map.addLayer(point1, {color:'red'},'point1')
+
+var x1 = -102.0006
+var y1 = 55.8126
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+Map.addLayer(point1, {color:'red'},'point1')
+
+var x1 = -101.9519
+var y1 = 55.8142
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+Map.addLayer(point1, {color:'red'},'point1')
+
+var x1 = -101.956
+var y1 = 55.4631
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+Map.addLayer(point1, {color:'red'},'point1')
+
+var x1 = -101.9148
+var y1 = 55.4619
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+Map.addLayer(point1, {color:'red'},'point1')
+
+var x1 = -101.921
+var y1 = 55.1104
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+Map.addLayer(point1, {color:'red'},'point1')
+
+var x1 = -101.8791
+var y1 = 55.1096
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.8861
+var y1 = 54.7623
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.847
+var y1 = 54.7639
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.8435
+var y1 = 54.7421
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.8511
+var y1 = 54.4149
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.8113
+var y1 = 54.4137
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.812
+var y1 = 54.4149
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.812
+var y1 = 54.0642
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.7694
+var y1 = 54.065
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.7708
+var y1 = 53.71
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.7433
+var y1 = 53.71
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.7354
+var y1 = 53.6231
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.73506
+var y1 = 53.52834
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.73738
+var y1 = 53.52834
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.73687
+var y1 = 53.36461
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.70237
+var y1 = 53.3644
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.67228
+var y1 = 53.01144
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.67099
+var y1 = 53.00017
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.67185
+var y1 = 53.00011
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.67068
+var y1 = 52.66699
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.63687
+var y1 = 52.66725
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.63788
+var y1 = 52.31834
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.60801
+var y1 = 52.31839
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.60743
+var y1 = 52.22078
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.60931
+var y1 = 52.20359
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.60955
+var y1 = 52.11675
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.60951
+var y1 = 51.9834
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.60844
+var y1 = 51.96915
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.60951
+var y1 = 51.96849
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.57754
+var y1 = 51.96854
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.57815
+var y1 = 51.92537
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.57733
+var y1 = 51.92251
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.5769
+var y1 = 51.9044
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.57778
+var y1 = 51.65509
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+var x1 = -101.57049
+var y1 = 51.65512
+var point1=ee.Geometry.Point(x1,y1)
+Map.centerObject(point1, 15)
+
+Map.addLayer(point1, {color:'red'},'point1')
+
+
+
+
+var polygon = ee.Geometry.Polygon([[x1,y1],[x2,y2],[x3,y3],[x4,y4],[x5,y5],[x6,y6],[x7,y7],[x8,y8],[x9,y9],[x10,y10],[x11,y11],[x12,y12],[x13,y13],[x14,y14],[x15,y15],[x16,y16],[x17,y17],[x18,y18],[x19,y19],[x20,y20],
+                                  [x21,y21],[x22,y22],[x23,y23],[x24,y24],[x25,y25],[x26,y26],[x27,y27],[x28,y28],[x29,y29],[x30,y30],[x31,y31],[x32,y32],[x33,y33],[x34,y34],[x35,y35],[x36,y36],[x37,y37],[x38,y39],
+                                  [x40,y40],[x41,y41],[x42,y42],[x43,y43],[x44,y44],[x45,y45],[x46,y46],[x47,y47],[x48,y48],[x49,y49],[x50,y50],[x51,y51],[x52,y52],[x53,y53],[x54,y54],[x55,y55],[x56,y56],[x57,y57],[x58,y58],
+                                  [x59,y59],[x60,y60],[x61,y61],[x62,y62],[x63,y63],[x64,y64],[x65,y65],[x66,y66],[x67,y67],[x68,y68],[x69,y69],[x70,y70],[x71,y71],[x72,y72],[x73,y73],[x74,y74],[x75,y75],[x76,y76],[x77,y77],
+                                  [x78,y78],[x79,y79],[x80,y80],[x81,y81],[x82,y82],[x83,y83],[x84,y84],[x85,y85],[x86,y86],[x87,y87],[x88,y88],[x89,y89],[x90,y90],[x91,y91],[x92,y92],[x93,y93],[x94,y94],[x95,y95],[x96,y96],
+                                  [x97,y97],[x98,y98],[x99,y99],[x100,y100]])
+
+Map.addLayer(polygon,{color:'blue'},'polygon')
